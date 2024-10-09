e52825b70809b2f5a86666f492809432ca2a9675
Greenwood <greenwood672.dev@gmail.com>
2024-10-09 20:57:06 +0100

    prevent removing console log

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: false,  // Prevents Terser from removing console logs
+            },
+        },
+    }
 });
